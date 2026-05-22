f502757dc3ecd75fbaa7cfc8854d13253b79b6d6
tahjfun-netizen <tahjfun@gmail.com>
2026-05-22 01:59:48 +0000

    chapter2/ex11 Task1

--- chapter2/ex11/student/ElectionStatistics.java
+++ chapter2/ex11/student/ElectionStatistics.java
@@ -1,1 +1,11 @@
-// Write your code here
+Import java.util.Scanner; 
+
+public class ElectionStatistics {
+    public static void main(String[] args) {
+        scanner input = new scanner(system.in)
+        
+        
+    }
+
+    
+}
