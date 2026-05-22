b59789a1cfde077f16519c721c91bc9d0212f9fe
tahjfun-netizen <tahjfun@gmail.com>
2026-05-22 00:19:07 +0000

    chapter1/ex06 Task2

--- chapter1/ex06/student/SongLyrics.java
+++ chapter1/ex06/student/SongLyrics.java
@@ -1,1 +1,8 @@
-// Write your code here
+public class SongLyrics {
+public static void main(String[] args) {
+System.out.println("all my life");
+System.out.println("They been trying to keep me down");
+System.out.println("All this time");
+System.out.println("Never thought i would make it out");
+}
+}
