fafa4afd33ddcb196ad3a7a2f1ba748a71d3aad9
tahjfun-netizen <tahjfun@gmail.com>
2026-05-22 02:36:09 +0000

    chapter2/de01 Task1

--- chapter2/de01/student/DebugTwo1.java
+++ chapter2/de01/student/DebugTwo1.java
@@ -4,7 +4,7 @@ public class DebugTwo1
    public static void main(String[] args)
    {
       int oneInt;
-      double oneDouble;
+      Double oneDouble;
       String oneString;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter an integer >> ");
@@ -16,7 +16,7 @@ public class DebugTwo1
       oneString = input.nextLine();
       System.out.print("The int is ");
       System.out.println(oneIntt);
-      System.out.print(The double is ");
+      System.out.print("The double is ");
       System.out.println(oneDouble);
       System.out.print("The String is ");
       System.out.println(oneString);
