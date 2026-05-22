c29d38c09bb760d51a6f7974e71e8cf1a17170fe
tahjfun-netizen <tahjfun@gmail.com>
2026-05-22 00:38:31 +0000

    chapter1/de01 Task1

--- chapter1/de01/student/DebugOne1.java
+++ chapter1/de01/student/DebugOne1.java
@@ -1,7 +1,6 @@
 public class DebugOne1
 {
-   /* This program displays a greeting
-   public void main(String[] args)
+ public void main(String[] args)
    {
       System.out.println("Hello");
    }
