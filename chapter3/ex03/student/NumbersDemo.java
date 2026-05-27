6ae37c45bd06b660a21400ca6a7cd9dd9d342d07
tahjfun-netizen <tahjfun@gmail.com>
2026-05-27 04:01:00 +0000

    chapter3/ex03 Task1

--- chapter3/ex03/student/NumbersDemo.java
+++ chapter3/ex03/student/NumbersDemo.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class NumbersDemo {
 
     public static void main(String[] args) {
-        Scanner input = new scanner (System.in);
-        system.out.print("enter another integer >>");
+        Scanner input = new Scanner (System.in);
+        System.out.print("enter another integer >>");
         int num1 = input.nextInt();
         System.out.print("Enter another integer");
         int num2 = inpt.nextInt();
-        
+
 
 
     }
