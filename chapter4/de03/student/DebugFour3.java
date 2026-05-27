6ed181777ffe1213c738d8ab77638bc265923f4d
tahjfun-netizen <tahjfun@gmail.com>
2026-05-27 21:16:59 +0000

    chapter4/de03 Task4

--- chapter4/de03/student/DebugFour3.java
+++ chapter4/de03/student/DebugFour3.java
@@ -15,11 +15,11 @@ public class DebugFour3
       h = input.nextInt();
       DebugBox box1 = new DebugBox();
       DebugBox box2 = new DebugBox(w, l, h);
-      System.out.println("The dimensions of the first box are");
+      System.out.println(" The dimensions of the first box are ");
       box1.showData();
       System.out.print("  The volume of the first box is ");
       showVolume(box1);
-      System.out.println("The dimensions of the second box are");
+      System.out.println(" The dimensions of the second box are ");
       box2.showData();
       System.out.print("  The volume of the second box is ");
       showVolume(box2);
