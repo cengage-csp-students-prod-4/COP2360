eaaeab974b32e3e21cc10597967055ebd690a054
tahjfun-netizen <tahjfun@gmail.com>
2026-06-02 21:54:52 +0000

    chapter5/ex04 Task1

--- chapter5/ex04/student/CellPhoneService.java
+++ chapter5/ex04/student/CellPhoneService.java
@@ -1,1 +1,34 @@
-// Write your code here
+import java.util.Scanner;
+public class CellPhoneService {
+    public static void main(String[] args) {
+        scanner input = new Scanner(System.in);
+     
+         
+        System.out.print("Enter maximum monthly talk minutes: ");
+        int minutes = input.nextInt();
+        System.out.print("Enter maximum monthly text minutes: ");
+         int texts = input.nextInt();
+         System.out.print("Enter maximum monthly gigabytes of data: ");
+         int data = input.nextInt();
+
+         String plan;
+         int price; 
+         if (data > 0) {
+            if (data < 3) {
+                plan = "plan E";
+                price =79;
+            } else {
+                plan = "Plan F";
+                price = 87;
+            }
+        }else if (minutes < 500) {
+            if (texts == 0) {
+                plan = "Plan A";
+                price = 
+        }
+
+         }
+
+    }
+    
+}lass
