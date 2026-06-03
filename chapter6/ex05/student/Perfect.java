6a7212e823f886d2ac189ca0a76b3362006aeb63
tahjfun-netizen <tahjfun@gmail.com>
2026-06-03 20:22:34 +0000

    chapter6/ex05 Task1

--- chapter6/ex05/student/Perfect.java
+++ chapter6/ex05/student/Perfect.java
@@ -1,1 +1,22 @@
 // Write your code here
+import java.util.Scanner;
+public class Perfect {}
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a maximum number >> "); 
+    int max = input.nextInt();
+
+    for (int number = 1; number <= max; number++) {
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == number) {
+            System.out.println("The number " + number + " is perfect");
+        }
+    }
+}
