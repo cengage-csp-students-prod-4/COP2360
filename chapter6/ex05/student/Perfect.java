82a8f3362d9c85dd50d19464518dcf0a0b9f3061
tahjfun-netizen <tahjfun@gmail.com>
2026-06-03 21:05:17 +0000

    chapter6/ex05 Task2

--- chapter6/ex05/student/Perfect.java
+++ chapter6/ex05/student/Perfect.java
@@ -12,14 +12,16 @@ public static void main(String[] args) {
             System.out.println("The number " + num + " is perfect");
         }
     }
-}
+
 public static boolean isPerfect(int n) {
     if (n < 2) return false;
     int sum = 0;
-    for (int i  1; i <= n / 2; i++) {
+    for (int i = 1; i <= n / 2; i++) {
         if (n % 1 == 0) {
             sum += i;
         }
     }
     return sum == n;
 }
+}
+
