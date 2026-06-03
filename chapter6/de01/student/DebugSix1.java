b4afde0abd901753b6b6a203d51a3793bb800980
tahjfun-netizen <tahjfun@gmail.com>
2026-06-03 19:43:05 +0000

    chapter6/de01 Task2

--- chapter6/de01/student/DebugSix1.java
+++ chapter6/de01/student/DebugSix1.java
@@ -10,9 +10,9 @@ public class DebugSix1
       int days;
       double money = 0.01;
       int day = 1;
-      System.out.print("Enter number of days >> ")
+      System.out.print("Enter number of days >> ");
       days = keyboard.nextInt();
-      while(days <= day)
+      while(days >= day)
       {
          money = 2 * money;
          ++day;
