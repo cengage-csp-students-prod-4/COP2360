4335332e27fd25b058e2e55d36ca5aeef6f3d6b6
tahjfun-netizen <tahjfun@gmail.com>
2026-06-18 05:08:36 +0000

    chapter9/ex03 Task8

--- chapter9/ex03/student/DemoTees.java
+++ chapter9/ex03/student/DemoTees.java
@@ -19,5 +19,48 @@ public class DemoTees
     System.out.print("Enter color >> ");
     shirt1.setColor(input.nextLine());
 
+    System.out.print("Enter size >> ");
+    shirt1.setSize(input.nextLine())
+
+     System.out.print("Enter order number >> ");
+    shirt2.setOrderNumber(input.nextLine()) 
+    input.nextLine();
+
+    System.out.print("Enter color >> ");
+    shirt2.setColor(input.nextLine());
+
+    System.out.print("Enter size >> ");
+    shirt2.setSize(input.nextLine());
+
+   System.out.print("Enter order number >> ");
+    custom1.setOrderNumber(input.nextInt());
+    input.nextLine();
+
+     System.out.print("Enter color >> ");
+    custom1.setColor(input.nextLine());
+
+    System.out.print("zEnter size >> ");
+    custom1.setSize(input.nextLine());
+
+    System.out.print("Enter slogan for shirt >> ");
+    custom1.setSlogan(input.nextLine());
+
+     System.out.print("Enter order number >> ");
+    custom2.setOrderNumber(input.nextInt());
+    input.nextLine();
+
+     System.out.print("Enter color >> ");
+    custom2.setColor(input.nextLine());
+
+    System.out.print("zEnter size >> ");
+    custom2.setSize(input.nextLine());
+
+    System.out.print("Enter slogan for shirt >> ");
+    custom2.setSlogan(input.nextLine());
+
+
     
+
+
+
 }
