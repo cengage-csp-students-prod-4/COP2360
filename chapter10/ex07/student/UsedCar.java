92fb5941ec8ce4f884301c5199df9cd0497a311d
tahjfun-netizen <tahjfun@gmail.com>
2026-06-18 17:11:59 +0000

    chapter10/ex07 Task1

--- chapter10/ex07/student/UsedCar.java
+++ chapter10/ex07/student/UsedCar.java
@@ -1,1 +1,62 @@
-// Write your code here
+public class UsedCar {
+    private String vin;
+    private String make;
+    private int year; 
+    private int mileage;
+    private double price;
+    public UsedCar(String vin, String make, int year, int mileage, double price)
+    throws UsedCarException {
+        if(vin.length() != 4)
+            throw new UsedCarException(vin);
+
+        boolean ValidMake = false;
+
+        if(make.equalsIgnoreCase("Ford"))
+            validMake = true;
+        else if(make.equalsIgnoreCase("Honda"))
+            validMake = true;
+         else if(make.equalsIgnoreCase("Toyota"))
+            validMake = true;
+         else if(make.equalsIgnoreCase("Chrysler"))
+            validMake = true;
+         else if(make.equalsIgnoreCase("Other"))
+            validMake = true;
+
+        if(!validMake)
+            throw new UsedCarException(vin);
+        if(year < 1997)
+            throw new UsedCarException(vin);
+        if(year < 2024)
+            throw new UsedCarException(vin);
+        if(mileage < 0)
+            throw new UsedCarException(vin);
+        if(price < 0)
+            throw new UsedCarException(vin);
+
+        this.vin = vin;
+        this.make = make;
+        this.year = year;
+        this.mileage = mileage;
+        this.price = price;
+    }
+    public String getVin()
+    {
+        return vin;
+    }
+    public String getMake()
+    {
+        return make;
+}
+    public int getYear()
+    {
+        return year;
+    }
+    public int getMileage()
+    {
+        return mileage;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+}
