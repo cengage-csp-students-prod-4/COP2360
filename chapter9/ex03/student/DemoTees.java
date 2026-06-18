feae01670dc4c3f274a02ccac978c52d33827ecb
tahjfun-netizen <tahjfun@gmail.com>
2026-06-18 04:46:50 +0000

    chapter9/ex03 Task8

--- chapter9/ex03/student/DemoTees.java
+++ chapter9/ex03/student/DemoTees.java
@@ -1,1 +1,17 @@
-// Write your code here
+import java.util.Scanner;
+
+public class DemoTees
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        TeeShirt shirt1 = new TeeShirt();
+        TeeShirt shirt2 = new TeeShirt();
+
+        CustomTee custom1 = new CustomTee();
+        CustomTee custom2 = new CustomTee();
+    
+    System.out.print("Enter order number >> ");
+    shirt1.setOrderNumber(input.nextInt())
+}
