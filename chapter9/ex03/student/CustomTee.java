177f64b0f6c7bce443aacc0490feaeac907a2e91
tahjfun-netizen <tahjfun@gmail.com>
2026-06-18 04:35:20 +0000

    chapter9/ex03 Task6

--- chapter9/ex03/student/CustomTee.java
+++ chapter9/ex03/student/CustomTee.java
@@ -1,1 +1,14 @@
-// Write your code here
+public class CustomTee extends TeeShirt
+{
+    private String slogan;
+   
+    public void setSlogan(String slogan)
+    {
+        this.slogan = slogan;
+    }
+    public String getSlogan()
+    {
+        return slogan;
+    }
+}
+    
