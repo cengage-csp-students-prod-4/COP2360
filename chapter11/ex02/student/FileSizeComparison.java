0ac726fbeef888d06244ea8a4f9513df39b23206
tahjfun-netizen <tahjfun@gmail.com>
2026-06-18 19:59:50 +0000

    chapter11/ex02 Task1

--- chapter11/ex02/student/FileSizeComparison.java
+++ chapter11/ex02/student/FileSizeComparison.java
@@ -1,1 +1,27 @@
-// Write your code here
+import java.io.File;
+import java.util.Scanner;
+
+public class FileSizeComparison
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+    
+        System.out.print("Enter name of text file >> ");
+        String textFileName = input.nextLine();
+
+        System.out.print("Enter the name of the Word file >> ");
+        Strong wordFileName = input.nextLine();
+
+        File textFile = new File(textFileName);
+        File wordFile = new File(wordFileName);
+
+        long textSize = textFile.length();
+        long wordSize = wordFile.length();
+
+        double percent = ((double) textSize / wordSize) * 100;
+        System.out.println(textFileName + " is " + textSize + " bytes long");
+        System.out.println("The text file is " + percent + "% of the size of the word file");
+        input.close();
+        }
+}
