7285e3b2a07ac9c66f0e3ac4cb1f902f3dc6df48
tahjfun-netizen <tahjfun@gmail.com>
2026-06-18 04:17:17 +0000

    chapter9/ex03 Task1

--- chapter9/ex03/student/TeeShirt.java
+++ chapter9/ex03/student/TeeShirt.java
@@ -1,1 +1,45 @@
-// Write your code here
+public class TeeShirt
+{
+    private int orderNumber;
+    private String size;
+    private String color;
+    private double price;
+
+    public void setOrderNumber(int orderNumber)
+    {
+        this.orderNumber = orderNumber;
+    }
+    public void setSize(String size)
+    {
+        this.size = size;
+        if(size.equalsIgnoreCase("XXL")) ||
+        size.equalsIgnoreCase("XXXL")
+        {
+            price = 22.99;
+        }
+        else
+        {
+            price = 19.99;
+        }
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+    public int getOrderNumber()
+    {
+        return orderNumber;
+    }
+    public String getSize()
+    {
+        return size;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+}
