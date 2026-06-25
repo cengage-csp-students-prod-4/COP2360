a15daf6334ea577d91fc93613ad44378bf8cef96
tahjfun-netizen <tahjfun@gmail.com>
2026-06-25 03:28:59 +0000

    chapter13/ex01 Task1

--- chapter13/ex01/student/Purchase.java
+++ chapter13/ex01/student/Purchase.java
@@ -1,1 +1,31 @@
-// Write your code here
+public class Purchase implements Comparable<Purchase>
+{
+    private String category;
+    private double price;
+
+    public Purchase(String category, double price)
+    {
+        this.category = category;
+        this.price = price;
+    }
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+    @Override
+    public int compareTo(Purchase other)
+    {
+        return category.compareTo(other.category);
+    }
+
+    @Override
+    public String toString()
+    {
+        return category + " " + price;
+    }
+}
---
+++ chapter13/ex01/student/PurchaseArrayListWithTotals.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class PurchaseArrayListWithTotals
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        ArrayList<Purchase> purchases = new ArrayList<Purchase>();
+
+        String category;
+        double price;
+        final String QUIT = "ZZZ";
+
+        System.out.print("Enter category for purchase or " + QUIT + "to quit >> ");
+        category = kb.nextLine();
+
+        while(!category.equals(QUIT))
+        {
+            System.out.print("Enter amount spent >> ");
+            price = kb.nextDouble();
+            kb.nextLine();
+
+            purchases.add(new Purchase(category, price));
+
+            System.out.print("Enter category for purchase or " + QUIT + " to quit >> ");
+            category = kb.nextLine();
+        }
+        
+        Collections.sort(purchases);
+
+        String currentCategory = "";
+        double total = 0;
+
+        for(int i = 0; i < purchases.size(); i++)
+        {
+            Purchase p = purchases.get(i);
+            
+            if(!p.getCategory().equals(currentCategory))
+            {
+                if(i > 0)
+                {
+                    System.out.println("   Total for " + currentCategory + ": " + total);
+                }
+
+                currentCategory = p.getCategory();
+                total = 0;
+            }
+
+            System.out.println(p);
+            total += p.getPrice();
+        }
+
+        if(purchases.size() > 0)
+        {
+            System.out.println("   Total for " +
+            currentCategory + ": " + total);
+        }
+}
+}
